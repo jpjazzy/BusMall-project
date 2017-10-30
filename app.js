1eec7ec7c738548971b146f05a89f1d413a8c235
JEREMY PEARSON <JPJAZZY@HOTMAIL.COM>
2017-10-30 15:57:57 -0700

    added js to display pictures

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var breakfast = new Product('breakfast', './img/breakfast.jpg');
 var bubblegum = new Product('bubblegum', './img/bubblegum.jpg');
 var chair = new Product('chair', './img/chair.jpg');
 var cthulhu = new Product('cthulhu', './img/cthulhu.jpg');
-var dogDuck = new Product('dog duck', './img/dog-duck');
+var dogDuck = new Product('dog duck', './img/dog-duck.jpg');
 var dragon = new Product('dragon', './img/dragon.jpg');
 var pen = new Product('pen', './img/pen.jpg');
 var petSweep = new Product('pet sweep', './img/pet-sweep.jpg');
@@ -42,16 +42,63 @@ var arrOfProd = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthu
 //Trial counter to know where the user is at
 var trialCounter = 0;
 
-//image numbers for the array (negative to start)
-var randImage1 = -1;
-var randImage2 = -1;
-var randImage3 = -1;
-
-//grab the image locations
+//grab the image locations and counter position
 var image1 = document.getElementById('image1');
 var image2 = document.getElementById('image2');
 var image3 = document.getElementById('image3');
+var progress = document.getElementById('progress');
 
-//generate 3 random numbers
-//check if images were previously displayed (if yes reroll image)
-//place images on page
+function displayRandomImages() {
+  // give a ticker +1 for objects selected
+  //image numbers for the array (negative to start)
+  var randImage1 = -1;
+  var randImage2 = -1;
+  var randImage3 = -1;
+
+  //check if trialcounter is less than 25
+  if (trialCounter > 25) {
+    return;
+  }
+  //increment trial counter
+  trialCounter++;
+  progress.innerHTML = 'Product trial ' + trialCounter + '/25';
+
+  //generate 3 random numbers
+  //check if images were previously displayed (if yes reroll image) with corresponding numbers
+  randImage1 = getRandomInt(0, arrOfProd.length);
+  while (arrOfProd[randImage1].justShown === true) {
+    randImage1 = getRandomInt(0, arrOfProd.length);
+  }
+
+  randImage2 = getRandomInt(0, arrOfProd.length);
+  while (arrOfProd[randImage2].justShown === true || randImage2 === randImage1) {
+    randImage2 = getRandomInt(0, arrOfProd.length);
+  }
+
+  randImage3 = getRandomInt(0, arrOfProd.length);
+  while (arrOfProd[randImage3].justShown === true || randImage3 === randImage2 || randImage3 === randImage1) {
+    randImage3 = getRandomInt(0, arrOfProd.length);
+  }
+
+  console.log('image 1 number:', randImage1);
+  console.log('image 2 number:', randImage2);
+  console.log('image 3 number:', randImage3);
+
+  //set flags to true for the next iteration
+  arrOfProd[randImage2].justShown = true;
+  arrOfProd[randImage1].justShown = true;
+  arrOfProd[randImage3].justShown = true;
+
+  //place images on page
+  image1.setAttribute('src', arrOfProd[randImage1].path);
+  image2.setAttribute('src', arrOfProd[randImage2].path);
+  image3.setAttribute('src', arrOfProd[randImage3].path);
+
+  //after 25 trials then display statistics
+}
+
+displayRandomImages();
+
+image1.addEventListener('click', displayRandomImages);
+image2.addEventListener('click', displayRandomImages);
+image3.addEventListener('click', displayRandomImages);
