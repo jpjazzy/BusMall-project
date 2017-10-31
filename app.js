1c2cbc537bd856b93f7d44bf1ccc2ad5e91ccd5b
JEREMY PEARSON <JPJAZZY@HOTMAIL.COM>
2017-10-30 17:36:01 -0700

    added functionality for statistics and images

--- app.js
+++ app.js
@@ -42,11 +42,15 @@ var arrOfProd = [bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthu
 //Trial counter to know where the user is at
 var trialCounter = 0;
 
+//How many trials should be in this study?
+var numOfTrials = 25;
+
 //grab the image locations and counter position
 var image1 = document.getElementById('image1');
 var image2 = document.getElementById('image2');
 var image3 = document.getElementById('image3');
 var progress = document.getElementById('progress');
+var imageContainer = document.getElementById('image_container');
 
 function displayRandomImages() {
   // give a ticker +1 for objects selected
@@ -56,9 +60,28 @@ function displayRandomImages() {
   var randImage3 = -1;
 
   //check if trialcounter is less than 25
-  if (trialCounter > 25) {
+  //after 25 trials (or assigned number) then display statistics
+  if (trialCounter === numOfTrials) {
+    var newPTag;
+    var prodPercent;
+    for (var j = 0; j < arrOfProd.length; j++) {
+      newPTag = document.createElement('p');
+      prodPercent = Math.round(arrOfProd[j].clicks / arrOfProd[j].totalShown * 100);
+      newPTag.innerHTML = 'Product: ' + arrOfProd[j].name + ', Number of times shown: ' + arrOfProd[j].totalShown + ', Number of times selected: ' + arrOfProd[j].clicks + ', % of time selected: ' + prodPercent + '%';
+      imageContainer.appendChild(newPTag);
+    }
+    image1.style.display = 'none';
+    image2.style.display = 'none';
+    image3.style.display = 'none';
+    progress.style.display = 'none';
+
+    //turn off event listeners
+    image1.removeEventListener('click', onClickData);
+    image2.removeEventListener('click', onClickData);
+    image3.removeEventListener('click', onClickData);
     return;
   }
+
   //increment trial counter
   trialCounter++;
   progress.innerHTML = 'Product trial ' + trialCounter + '/25';
@@ -84,21 +107,43 @@ function displayRandomImages() {
   console.log('image 2 number:', randImage2);
   console.log('image 3 number:', randImage3);
 
-  //set flags to true for the next iteration
+  //set flags to true for the next iteration and remove old flags
+  for (var i = 0; i < arrOfProd.length; i++) {
+    arrOfProd[i].justShown = false;
+  }
+
   arrOfProd[randImage2].justShown = true;
   arrOfProd[randImage1].justShown = true;
   arrOfProd[randImage3].justShown = true;
+
+  //increment total times shown on displayed images
+  arrOfProd[randImage2].totalShown++;
+  arrOfProd[randImage1].totalShown++;
+  arrOfProd[randImage3].totalShown++;
 
   //place images on page
   image1.setAttribute('src', arrOfProd[randImage1].path);
   image2.setAttribute('src', arrOfProd[randImage2].path);
   image3.setAttribute('src', arrOfProd[randImage3].path);
+}
 
-  //after 25 trials then display statistics
+//perform on click function
+function onClickData(e) {
+  console.log(e.target.getAttribute('src'));
+
+  for (var i = 0; i < arrOfProd.length; i++) {
+    if (e.target.getAttribute('src') === arrOfProd[i].path) {
+      arrOfProd[i].clicks++;
+    }
+  }
+
+  //display more images or statistics
+  displayRandomImages();
+
 }
 
 displayRandomImages();
 
-image1.addEventListener('click', displayRandomImages);
-image2.addEventListener('click', displayRandomImages);
-image3.addEventListener('click', displayRandomImages);
+image1.addEventListener('click', onClickData);
+image2.addEventListener('click', onClickData);
+image3.addEventListener('click', onClickData);
